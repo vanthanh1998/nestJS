5f7bf99c0959298e1feb94aec3178a131ce96906
Van <foxrainsad@gmail.com>
2024-12-15 22:05:10 +0700

    #2: hash password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,16 +4,26 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
 
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
+  // hash pw
+  getHashPassword = (password: string) => {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  }
+
   // create(createUserDto: CreateUserDto) {
   async create(email: string, password: string, name: string) {
+    const hashPassword = this.getHashPassword(password)
+
     let user = await this.userModel.create({
-      email, password, name
+      email, password: hashPassword, name
     })
     return user;
   }
