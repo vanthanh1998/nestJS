d8ef013bbf3f80eb7103d74e3a40921589d57039
Van <foxrainsad@gmail.com>
2024-12-16 23:05:28 +0700

    #4: validation use class-validator

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,14 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 // data transfer object // class = {}
 export class CreateUserDto {
+    @IsEmail({}, { message: 'Email k đúng định dạng', })
+    @IsNotEmpty({ message: 'Email k đc để trống', })
     email: string;
+
+    @IsNotEmpty({ message: 'Password k đc để trống', })
     password: string;
+
     name: string;
     address: string;
 }
