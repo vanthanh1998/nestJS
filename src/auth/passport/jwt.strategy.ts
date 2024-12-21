9ebb6ad5304b31d011147653572ca4b95fef57ef
Van <foxrainsad@gmail.com>
2024-12-21 11:29:59 +0700

    #5: package ms

--- src/auth/passport/jwt.strategy.ts
+++ src/auth/passport/jwt.strategy.ts
@@ -12,11 +12,11 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({ // đoạn này giúp giải mã token jwt
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET'),
+      secretOrKey: configService.get<string>('JWT_ACCESS_TOKEN'),
     });
   }
 
-  async validate(payload: any) {
+  async validate(payload: any) { // hàm validate này của package jwt => nếu token hợp lệ thì trả ra data như này
     return { 
         userId: payload.sub, 
         username: payload.username,
