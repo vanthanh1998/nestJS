7b5dd0d9be8bdee1ca2b50e5a9b26bae00b353fe
Van <foxrainsad@gmail.com>
2024-12-22 12:43:34 +0700

    #3: Move function login in app.controller => auth.controller + create interface

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -23,7 +23,7 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
   handleRequest(err, user, info) {
     // You can throw an exception based on either "info" or "err" arguments
     if (err || !user) {
-      throw err || new UnauthorizedException("Token not found");
+      throw err || new UnauthorizedException("Token or bearer token not found ");
     }
     return user;
   }
