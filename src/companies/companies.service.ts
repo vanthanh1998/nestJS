102c33ac1a1e91822776e7f7d2dfb69d90d06338
Van <foxrainsad@gmail.com>
2024-12-22 12:17:30 +0700

    #2: create companies

--- src/companies/companies.service.ts
+++ src/companies/companies.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Company, CompanyDocument } from './schemas/company.schema';
+import { SoftDeleteModel } from 'soft-delete-plugin-mongoose';
 
 @Injectable()
 export class CompaniesService {
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+
+  constructor(
+    @InjectModel(Company.name) 
+    private companyModel: SoftDeleteModel<CompanyDocument>
+  ) {}
+
+  async create(createCompanyDto: CreateCompanyDto) {
+    // let company = this.companyModel.create({
+    //   name: createCompanyDto.name,
+    //   address: createCompanyDto.address,
+    //   desc: createCompanyDto.desc,
+    // })
+
+    let company = this.companyModel.create({ ...createCompanyDto })
+
+    return company;
   }
 
   findAll() {
