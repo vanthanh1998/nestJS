a3d1e5460ce44a242d761822d06539f674bafeef
Van <foxrainsad@gmail.com>
2024-12-26 14:44:20 +0700

    #2: generate upload files

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { AuthModule } from './auth/auth.module';
 import { softDeletePlugin } from 'soft-delete-plugin-mongoose';
 import { CompaniesModule } from './companies/companies.module';
 import { JobsModule } from './jobs/jobs.module';
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -30,7 +31,8 @@ import { JobsModule } from './jobs/jobs.module';
     UsersModule,
     AuthModule,
     CompaniesModule,
-    JobsModule
+    JobsModule,
+    FilesModule
   ],
   controllers: [AppController],
   providers: [AppService],
