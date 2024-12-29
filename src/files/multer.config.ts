c1e85357380d21351f0bb64d469879fdba890bc7
Van <foxrainsad@gmail.com>
2024-12-29 12:20:24 +0700

    #5: Fix bug upload file

--- src/files/multer.config.ts
+++ src/files/multer.config.ts
@@ -1,4 +1,4 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, HttpStatus, Injectable } from "@nestjs/common";
 import { MulterModuleOptions, MulterOptionsFactory } from "@nestjs/platform-express";
 import fs from 'fs';
 import { diskStorage } from "multer";
@@ -9,30 +9,31 @@ export class MulterConfigService implements MulterOptionsFactory {
     getRootPath = () => {
         return process.cwd();
     };
-    
+
     ensureExists(targetDirectory: string) {
         fs.mkdir(targetDirectory, { recursive: true }, (error) => {
-        if (!error) {
-            console.log('Directory successfully created, or it already exists.');
-            return;
-        }
-        switch (error.code) {
-            case 'EEXIST':
-            // Error:
-            // Requested location already exists, but it's not a directory.
-                break;
-            case 'ENOTDIR':
-            // Error:
-            // The parent hierarchy contains a file with the same name as the dir
-            // you're trying to create.
-                break;
-            default:
-            // Some other error like permission denied.
-                console.error(error);
-                break;
+            if (!error) {
+                console.log('Directory successfully created, or it already exists.');
+                return;
+            }
+            switch (error.code) {
+                case 'EEXIST':
+                    // Error:
+                    // Requested location already exists, but it's not a directory.
+                    break;
+                case 'ENOTDIR':
+                    // Error:
+                    // The parent hierarchy contains a file with the same name as the dir
+                    // you're trying to create.
+                    break;
+                default:
+                    // Some other error like permission denied.
+                    console.error(error);
+                    break;
             }
         });
     }
+
     createMulterOptions(): MulterModuleOptions {
         return {
             storage: diskStorage({
@@ -47,11 +48,26 @@ export class MulterConfigService implements MulterOptionsFactory {
 
                     //get image's name (without extension)
                     let baseName = path.basename(file.originalname, extName);
-                    
+
                     let finalName = `${baseName}-${Date.now()}${extName}`
                     cb(null, finalName)
-                }
-            })
+                },
+
+            }),
+            fileFilter: (req, file, cb) => {
+                const allowedFileTypes = ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'doc', 'docx'];
+                const fileExtension = file.originalname.split('.').pop().toLowerCase();
+                const isValidFileType = allowedFileTypes.includes(fileExtension);
+
+                if (!isValidFileType) {
+                    cb(new HttpException('Invalid file type', HttpStatus.UNPROCESSABLE_ENTITY), null);
+                } else
+                    cb(null, true);
+            },
+            limits: {
+                fileSize: 1024 * 1024 * 1 // 1MB
+            }
         };
     }
+
 }
