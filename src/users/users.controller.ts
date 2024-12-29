32c9186f98f3abfa34f408f4eecd6f1d3e8afa1a
Van <foxrainsad@gmail.com>
2024-12-29 13:24:54 +0700

    #7: fix error when update users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -17,10 +17,14 @@ export class UsersController {
     return this.usersService.create(createUserDto, user);
   }
 
-  @Patch()
+  @Patch(':id')
   @ResponseMessage("Update a User")
-  update(@Body() updateUserDto: UpdateUserDto, @User() user: IUser) {
-    return this.usersService.update(updateUserDto, user);
+  update(
+    @Param('id') id: string,
+    @Body() updateUserDto: UpdateUserDto, 
+    @User() user: IUser
+  ) {
+    return this.usersService.update(id, updateUserDto, user);
   }
 
   @Delete(':id')
